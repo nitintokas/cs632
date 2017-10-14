9e736e260dcbdc47c0ed49058c184d129e3b171a
nt69036n <32689935+nt69036n@users.noreply.github.com>
2017-10-14 17:47:39 -0400

    moving file

--- hw1/part2.py
+++ hw1/part2.py
@@ -1,10 +1,3 @@
-
-# coding: utf-8
-
-# # import glob for iterating files in folder and use dataframe for structuring
-
-# In[1]:
-
 import glob
 import pandas as pd
 
@@ -21,25 +14,13 @@ for fileName in list_of_files:
 body_file = pd.DataFrame(data = body,columns = ['post'])
 body_file
 
-
-
-# # extract label file
-
-# In[3]:
-
-#list_of_labels = glob.glob('/Users/ajinkya.parkar@ibm.com/Documents/deep/spam_data/labels.txt')
 label_file = pd.read_csv('spam_data/labels.txt',header = None,delimiter=" ",names=["label",'Name'])
 label_file 
 
 
-# In[4]:
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-
-# In[5]:
 
 import itertools
 import os
@@ -59,28 +40,13 @@ from tensorflow.contrib.keras.python.keras.layers import Dense, Activation, Drop
 from tensorflow.contrib.keras.python.keras.preprocessing import text, sequence
 from tensorflow.contrib.keras.python.keras import utils
 
-# This code was tested with TensorFlow v1.3
-print("You have TensorFlow version", tf.__version__)
-
-
-# In[6]:
-
 body_file["post"]
-
-
-# # Set index for train and test
-
-# In[7]:
-
-# Split data into train and test
 train_size = int(len(body_file) * .5)
 print ("Train size: %d" % train_size)
 print ("Test size: %d" % (len(body_file) - train_size))
 
 
 # # Prepare train and test data in 50:50 ratio
-
-# In[8]:
 
 
 train_posts = body_file['post'][:train_size]
@@ -90,51 +56,17 @@ test_posts = body_file['post'][train_size:]
 test_tags = label_file['label'][train_size:]
 
 
-# In[9]:
-
 train_posts
 
 
-# File 1) a bunch of text
-# Label 1) spam
-# 
-# File 2) a bunch more text
-# Label 2) not spam
-# 
-# 
-# more text
-# 
-# x_train 1) 0, 1
-# x_train 2) 1, 1
-# 
-# testing example 1) more text
-# x_test 1) 1, 1
-# 
-# 
-# 
-# 
-
-# In[10]:
 
 len(train_posts), len(train_tags)
 
 
-# In[11]:
-
 len(test_posts), len(test_tags)
-
-
-# # Set bag of words array length
-
-# In[12]:
 
 max_words = 10
 tokenize = text.Tokenizer(num_words=max_words, char_level=False)
-
-
-# # train posts
-
-# In[13]:
 
 tokenize.fit_on_texts(train_posts) # only fit on train
 
@@ -142,11 +74,7 @@ tokenize.fit_on_texts(train_posts) # only fit on train
 # In[14]:
 
 text = "the"
-# tokenize the text, and retrieve the index of the first 10 tokens
 tokenize.texts_to_matrix([text])
-
-
-# In[15]:
 
 x_train = tokenize.texts_to_matrix(train_posts)
 x_test = tokenize.texts_to_matrix(test_posts)
